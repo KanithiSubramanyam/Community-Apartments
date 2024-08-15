dcf755a228371c743e09ce319cf56f688a4f5139
KantihiSubramanyam <subramanyamkanithi@gmail.com>
2024-08-15 16:02:27 +0530

    navbar changes completed

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FormsModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  isScrolled = false;
+
+  ngOnInit() {
+    // Initialize isScrolled based on the current scroll position
+    this.isScrolled = window.scrollY > 100;
+  }
+  
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if (window.scrollY > 100 || window.pageYOffset > 100) {
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = false;
+    }
+  }
 }
