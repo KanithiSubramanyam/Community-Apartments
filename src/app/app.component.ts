3dab3c23416f5127bc1445888fe194e38a955373
gnaneswara123 <gnaneswar.petla@gmail.com>
2024-08-16 01:01:25 +0530

    apartments added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ApartmentsComponent } from './apartments/apartments.component';
 import { AmenitiesComponent } from './amenities/amenities.component';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactComponent } from './contact/contact.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent,AmenitiesComponent, AboutComponent, NavbarComponent, FooterComponent, ContactComponent],
+  imports: [RouterOutlet, HomeComponent,AmenitiesComponent, ApartmentsComponent,AboutComponent, NavbarComponent, FooterComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ApartmentsComponent } from './apartments/apartments.component';
 import { AmenitiesComponent } from './amenities/amenities.component';
 import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path : '', component : HomeComponent},
     {path : 'about', component : AboutComponent},
+    {path: 'apartments', component:ApartmentsComponent},
     {path : 'amenities', component : AmenitiesComponent},
     {path : 'contact', component : ContactComponent},
     {path : '**', component : HomeComponent}
