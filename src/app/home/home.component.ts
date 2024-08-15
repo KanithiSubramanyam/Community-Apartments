84dff958244dcefba34272d99a11f914ead42d9f
KantihiSubramanyam <subramanyamkanithi@gmail.com>
2024-08-16 00:22:04 +0530

    home page is completed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
   homePage = "assets/homepage.jpg";
-
+  aboutUs1 = "assets/interior-2.jpg";
+  aboutUs2 = "assets/homeAboutUs-2.jpg";
+  hotelIcon = "assets/hotelIcon.png";
+  aboutUsBackground = "assets/aboutUsBackground.jpg";
+  interior1 = "assets/interior-1.jpg";
 }
