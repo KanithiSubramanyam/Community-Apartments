0a8e6a275d34b4d8a07e89794d22d2ea8510a3b5
KantihiSubramanyam <subramanyamkanithi@gmail.com>
2024-08-17 11:57:43 +0530

    about us page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CounterComponent } from './counter/counter.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CounterComponent, RouterLink],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  aboutUs1 = "assets/interior-2.jpg";
+  aboutUs2 = "assets/homeAboutUs-2.jpg";
+  hotelIcon = "assets/hotelIcon.png";
+  aboutUsBreadcome = "assets/aboutUsBreadcome.jpg";
+  aboutUsAchivements = "assets/aboutUsAchivements.jpg";
+  pp = "assets/pp.jpg";
 }
